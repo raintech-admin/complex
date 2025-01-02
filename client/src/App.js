f7841d04d243ee888f713d882f2f375f8529c0e8
C.T. <>
2025-01-02 20:31:41 +0700

    no message

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fibonacci Calculator
+            Fibonacci Calculator App
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
